c1a5f6fe5a9682f9c04983838dbe8f22497d253e
tanulo <tanulo@barossgabor.local>
2025-11-17 14:11:34 +0100

    topjofetchkesz

--- 7_topjoy_fetch/script.js
+++ 7_topjoy_fetch/script.js
@@ -3,8 +3,13 @@ const messagebox = document.querySelector('#massagebox');
 const messagebutton = document.querySelector('#massageB');
 window.addEventListener('DOMContentLoaded', async () => {
    try {
-    const uzik = await fetch('./uzik.csv')
-    console.log(uzik)
+    const uzik = await fetch('./uzik.csv');
+    const UzikText = (await uzik.text()).split('\n').map((elem) => elem.trim());
+    UzikText.forEach(uzi => {
+      messages.push(uzi);
+    });
+    console.log(messages)
+    messagebutton.addEventListener("click", () =>{ const randomindex = messages[Math.floor(Math.random() * messages.length)]; messagebox.textContent = randomindex; console.log(randomindex);})
    } catch (error) {
     console.error(error)    
    }
