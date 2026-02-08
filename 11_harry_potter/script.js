28630d4719daa0dacbfbfa2673744ad4549915e4
SanyokA <sanyokaforever@gmail.com>
2026-02-08 10:57:58 +0100

    HarryPotterNincsKep

--- 11_harry_potter/script.js
+++ 11_harry_potter/script.js
@@ -1,5 +1,5 @@
 const karakter = document.querySelector('#karakterek')
-// console.log(karakter);
+// // console.log(karakter);
 const URL1 =  'https://raw.githubusercontent.com/Laboratoria/LIM011-data-lovers/master/src/data/potter/potter.json'
 window.addEventListener('DOMContentLoaded', fetchCharacters)
 async function fetchCharacters(){
@@ -16,6 +16,22 @@ async function fetchCharacters(){
 function displayCharacters(chars){
     // console.log(chars);
     chars.forEach(char => {
-        
+        const card = document.createElement('div')
+        const p = document.createElement('p')
+        const image = document.createElement('img')
+        const div2 = document.createElement('div')
+        const h3 = document.createElement('h3')
+        card.className = 'card'
+        image.src = char.image 
+        image.alt = char.name
+        div2.className = 'content'
+        h3.textContent = char.name
+        p.innerHTML = `Ház: ${char.house}<br> Születési év: ${char.yearOfBirth}<br> Színész: ${char.actor}`
+        div2.append(h3)
+        div2.append(p)
+        card.append(image)
+        card.append(div2)
+        karakter.append(card)
+        console.log(card)
     });
 }
